0500cc7153f825ac815a67eaa5799cec8c5cdce5
kursun12 <salih_kara@hotmail.nl>
2025-09-10 22:07:15 +0200

    Add React-based MCQ quiz with styled interface

---
+++ mcqproject/src/questions.js
@@ -0,0 +1,22 @@
+const questions = [
+  {
+    id: 1,
+    question: 'What is the capital of France?',
+    options: ['Berlin', 'Paris', 'Rome', 'Madrid'],
+    answer: 1,
+  },
+  {
+    id: 2,
+    question: 'Which planet is known as the Red Planet?',
+    options: ['Earth', 'Mars', 'Jupiter', 'Venus'],
+    answer: 1,
+  },
+  {
+    id: 3,
+    question: 'What is 2 + 2?',
+    options: ['3', '4', '5', '6'],
+    answer: 1,
+  },
+];
+
+export default questions;
