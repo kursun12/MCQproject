a04f27e44097c681566c912f48f63a5c44d5b88f
kursun12 <salih_kara@hotmail.nl>
2025-09-14 15:55:24 +0200

    Include additional question sets in default seed

--- mcqproject/src/main.jsx
+++ mcqproject/src/main.jsx
@@ -2,12 +2,32 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { set1, set2, set3, set4, set5 } from './questions.js';
+import {
+  set1,
+  set2,
+  set3,
+  set4,
+  set5,
+  set6,
+  set7,
+  set8,
+  set9,
+} from './questions.js';
 
 // On first load, populate localStorage with the bundled question sets so
 // users have a ready-to-use library without needing to import anything.
-const defaultSets = { set1, set2, set3, set4, set5 };
-const allQuestions = [...set1, ...set2, ...set3, ...set4, ...set5];
+const defaultSets = { set1, set2, set3, set4, set5, set6, set7, set8, set9 };
+const allQuestions = [
+  ...set1,
+  ...set2,
+  ...set3,
+  ...set4,
+  ...set5,
+  ...set6,
+  ...set7,
+  ...set8,
+  ...set9,
+];
 const LS_Q_KEY = 'questions';
 const LS_S_KEY = 'sets';
 
